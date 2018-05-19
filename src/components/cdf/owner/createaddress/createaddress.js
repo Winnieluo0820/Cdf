dc8784eec8ea67f9c3a0f64c58f69d863e31791b
jiejie <1064286593@qq.com>
2018-05-19 17:22:10 +0800

    三级联动

--- src/components/cdf/owner/createaddress/createaddress.js
+++ src/components/cdf/owner/createaddress/createaddress.js
@@ -4,13 +4,19 @@ import './createaddress.scss'
 import { Link } from 'react-router'
 import http from '../../../../utils/httpclient'
 
+import jQuery from 'jquery'
 
+import areaData from '../../../../utils/area.js'
 
 export default class CreateaddressComponent extends React.Component {
     state = {
         ipNumber: '',
         adderss: '',
-        userName: ''
+        userName: '',
+        data: areaData.allAreaInfo,
+        provinceIdx:18,
+        cityIdx:0,
+        areaIdx:0
     }
 
     inputChange(target, event) {
@@ -91,6 +97,8 @@ export default class CreateaddressComponent extends React.Component {
             var _ipNumber = log_ipNumber.value * 1;
             var _adderss = log_address.value;
             var _userName = log_ipName.value;
+            var changeDefaultText = document.querySelector('#createbox .defaultsAddress .left span').innerText;
+            _adderss = changeDefaultText + _adderss;
 
             var tips = document.querySelectorAll('.login_main .tip');
 
@@ -106,7 +114,7 @@ export default class CreateaddressComponent extends React.Component {
             http.post('addAddress', {
                 userName: _userName,
                 ipNumber: _ipNumber,
-                address: _adderss
+                address: _adderss 
             }).then((res) => {
                 if(res.status){
                     self.props.router.push('owner/address')
@@ -114,11 +122,108 @@ export default class CreateaddressComponent extends React.Component {
             })
         }
 
-        
+        //三级联动
+        jQuery(function($){
+            var $overlay = $('#createbox .overlay');
+            var $selectDefault = $('#createbox .selectDefault');
+            var $confirmArea = $('#createbox .confirmArea')
+            var $confirmAreaBtn = $confirmArea.find('button')
+            var $changeDefaultBtn = $('#changeDefault');
+            var $changeDefaultText  = $changeDefaultBtn.parent().parent().find('span');
+            var $provinceUl = $selectDefault.find('.province');
+            var $cityUl = $selectDefault.find('.city');
+            var $areaUl = $selectDefault.find('.areas');
+
+            $changeDefaultBtn.on('click',function(){
+                $overlay.fadeIn()
+                $selectDefault.css('display', 'flex');
+                $confirmArea.css('display','block'); 
+                
+                var provinceUlscrollBig = ($provinceUl.height() / ($provinceUl.find('li').eq(0).outerHeight() * $provinceUl.find('li').length)) * $provinceUl.get(0).scrollHeight;
+                var cityUlscrollBig = ($cityUl.height() / ($cityUl.find('li').eq(0).outerHeight() * $cityUl.find('li').length)) * $cityUl.get(0).scrollHeight;
+                var areaUlscrollBig = ($areaUl.height() / ($areaUl.find('li').eq(0).outerHeight() * $areaUl.find('li').length)) * $areaUl.get(0).scrollHeight;
+
+                var targetProvince = (provinceUlscrollBig / 3) * (self.state.provinceIdx);
+                $provinceUl.get(0).scrollTo(0,targetProvince)
+
+                var targetCity = (cityUlscrollBig / 3) * (self.state.cityIdx);
+                $cityUl.get(0).scrollTo(0,targetCity)
+
+                var targetArea = (areaUlscrollBig / 3) * (self.state.areaIdx);
+                $areaUl.get(0).scrollTo(0,targetArea)
+
+            })
+                
+            $provinceUl.on('click', 'li', function(event){
+                if($(this).text() == '---'){
+                    return false;
+                }
+                $(this).addClass('active').siblings().removeClass('active')
+                
+                var targetIdx;
+                $(this).parent().find('li').map(function(idx, item){
+                    if($(item).hasClass('active')){
+                        targetIdx = idx - 1;
+                    }
+                })
+                self.setState({
+                    provinceIdx: targetIdx ,
+                    cityIdx : 0 ,
+                    areaIdx : 0
+                })
+
+                $cityUl.get(0).scrollTo(0,0);
+                $areaUl.get(0).scrollTo(0,0);
+            })
+            $cityUl.on('click', 'li', function(event){
+                if($(this).text() == '---'){
+                    return false;
+                }
+                $(this).addClass('active').siblings().removeClass('active')
+
+                var targetIdx;
+                $(this).parent().find('li').map(function(idx, item){
+                    if($(item).hasClass('active')){
+                        targetIdx = idx - 1;
+                    }
+                })
+                self.setState({
+                    cityIdx : targetIdx ,
+                    areaIdx : 0
+                })
+
+                $areaUl.get(0).scrollTo(0,0);
+            })
+            $areaUl.on('click', 'li', function(event){
+                if($(this).text() == '---'){
+                    return false;
+                }
+                $(this).addClass('active').siblings().removeClass('active')
+
+                var targetIdx;
+                $(this).parent().find('li').map(function(idx, item){
+                    if($(item).hasClass('active')){
+                        targetIdx = idx - 1;
+                    }
+                })
+                self.setState({
+                    areaIdx : targetIdx
+                })
+            })
+
+            $confirmAreaBtn.on('click',function(){
+                var text = self.state.data[self.state.provinceIdx].name + self.state.data[self.state.provinceIdx].city[self.state.cityIdx].name + self.state.data[self.state.provinceIdx].city[self.state.cityIdx].area[self.state.areaIdx];
+                $overlay.fadeOut()
+                $selectDefault.css('display', 'none');
+                $confirmArea.css('display','none');
+                $changeDefaultText.text(text);
+            })
+        })
 
 
     }
     render() {
+        var self = this;
         return(
             <div id="createbox" className="animate-route">
                 <div className="createbox_top">
@@ -126,34 +231,89 @@ export default class CreateaddressComponent extends React.Component {
                     新增提货人信息
                     <span></span>
                 </div>
-                <div className="overlay"></div>
                 <div className="createbox_mian">
-                
-                     <main className="login_main">
-                    <div className="form_group">
-                        <input type="text" placeholder="前输入提货人的真实姓名"   value={this.state.userName} id="log_ipName" onChange={this.inputChange.bind(this,'userName')}/>
-                        <i className="iconfont icon-icon-2 tip"></i>
-                        <div className="form_tip"><i></i><span></span></div>
-                    </div>
-                    <div className="form_group">
-                      <input type="text" placeholder="请输入提货人的手机号" value={this.state.ipNumber} id="log_ipNumber" value={this.state.ipNumber} onChange={this.inputChange.bind(this,'ipNumber')}/>
-                        <i className="iconfont icon-icon-2 tip"></i>
-                        <div className="form_tip"><i></i><span></span></div>
-                    </div>
-                    
-                    <div className="form_group">
-                      <input type="text" placeholder="请输入提货人的详细地址" id="log_address" value={this.state.adderss} onChange={this.inputChange.bind(this,'adderss')} />
-                        <i className="iconfont icon-icon-2 tip"></i>
-                        <div className="form_tip"><i></i><span></span></div>
-                    </div>
-                    <div className="form_group">
-                        <button id="btn_login">保存提货人信息</button>
-                        <div className="alert"><i></i><span></span></div>
-                    </div>
-                  
-                </main>
+                    <main className="login_main">
+                        <div className="form_group">
+                            <input type="text" placeholder="前输入提货人的真实姓名"   value={this.state.userName} id="log_ipName" onChange={this.inputChange.bind(this,'userName')}/>
+                            <i className="iconfont icon-icon-2 tip"></i>
+                            <div className="form_tip"><i></i><span></span></div>
+                        </div>
+                        <div className="form_group">
+                          <input type="text" placeholder="请输入提货人的手机号" value={this.state.ipNumber} id="log_ipNumber" value={this.state.ipNumber} onChange={this.inputChange.bind(this,'ipNumber')}/>
+                            <i className="iconfont icon-icon-2 tip"></i>
+                            <div className="form_tip"><i></i><span></span></div>
+                        </div>      
+                        <div className="defaultsAddress">
+                            <div className="left">地址：&nbsp;<span>广东省广州市荔湾区</span></div>
+                            <div className="right">
+                                <button id="changeDefault">更改</button>
+                            </div>
+                        </div>           
+                        <div className="form_group">
+                          <input type="text" placeholder="请输入提货人的详细地址" id="log_address" value={this.state.adderss} onChange={this.inputChange.bind(this,'adderss')} />
+                            <i className="iconfont icon-icon-2 tip"></i>
+                            <div className="form_tip"><i></i><span></span></div>
+                        </div>
+                        <div className="form_group">
+                            <button id="btn_login">保存提货人信息</button>
+                            <div className="alert"><i></i><span></span></div>
+                        </div>  
+                    </main>
                 </div>
-
+                <div className="overlay"></div>
+                <div className="selectDefault">
+                    <ul className="province select">
+                        <li>---</li>
+                        {
+                            this.state.data.map(function(item,idx){
+                                if(idx == self.state.provinceIdx){
+                                    return (
+                                        <li className="active" key={item + idx}>{item.name}</li>
+                                    )
+                                }
+                                return (
+                                    <li key={item + idx}>{item.name}</li>
+                                )
+                            })
+                        }
+                        <li>---</li>
+                    </ul>
+                    <ul className="city select">
+                        <li>---</li>
+                        {
+                            this.state.data[this.state.provinceIdx].city.map(function(item,idx){
+                                if(idx == self.state.cityIdx){
+                                    return (
+                                        <li className="active" key={item + idx}>{item.name}</li>
+                                    )
+                                }
+                                return (
+                                    <li key={item + idx}>{item.name}</li>
+                                )
+                            })
+                        }
+                        <li>---</li>            
+                    </ul>
+                    <ul className="areas select">
+                        <li>---</li>
+                        {
+                            this.state.data[this.state.provinceIdx].city[this.state.cityIdx].area.map(function(item,idx){
+                                if(idx == self.state.areaIdx){
+                                    return (
+                                        <li className="active" key={item + idx}>{item}</li>
+                                    )
+                                }
+                                return (
+                                    <li key={item + idx}>{item}</li>
+                                )
+                            })
+                        }
+                        <li>---</li>
+                    </ul>
+                </div>
+                <div className="confirmArea">
+                    <button>确定</button>
+                </div>
             </div>
         )
 
